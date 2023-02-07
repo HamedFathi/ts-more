f7cdd0445ca437e73bc45c437fe7400cc21819ea
hamedfathi <hamedfathi@outlook.com>
2023-02-07 20:19:11 +0100

    feat(utilities): new groupBy() added.

--- src/utilities/array-utility.ts
+++ src/utilities/array-utility.ts
@@ -105,3 +105,20 @@ export function flattenRecursively<T>(
   }
   return result;
 }
+
+export function groupBy<K, V>(
+  items: Array<V>,
+  selector: (input: V) => K
+): Map<K, Array<V>> {
+  const map = new Map<K, Array<V>>();
+  items.forEach((item) => {
+    const key = selector(item);
+    const collection = map.get(key);
+    if (!collection) {
+      map.set(key, [item]);
+    } else {
+      collection.push(item);
+    }
+  });
+  return map;
+}
